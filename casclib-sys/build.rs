234e3bb6c76fcbe80c15482fb46599678583f136
Flux Xu <fluxxu@gmail.com>
2020-03-21 16:44:01 -0400

    chore: Use static CascLib.

--- casclib-sys/build.rs
+++ casclib-sys/build.rs
@@ -9,10 +9,21 @@ fn main() {
     println!("cargo:rerun-if-changed={}", casclib_path);
 
     // Builds CascLib using cmake
-    let dst = cmake::Config::new(&casclib_path).build();
+    let dst = cmake::Config::new(&casclib_path)
+        .define("CASC_BUILD_SHARED_LIB", "OFF")
+        .define("CASC_BUILD_STATIC_LIB", "ON")
+        .build();
 
     let lib = dst.join("lib");
 
+    let target = env::var("TARGET").unwrap();
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=dylib=c++");
+        println!("cargo:rustc-link-lib=z");
+    } else if target.contains("linux") {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    }
+
     println!("cargo:rustc-link-search=native={}", lib.display());
-    println!("cargo:rustc-link-lib=casc");
+    println!("cargo:rustc-link-lib=static=casc");
 }
